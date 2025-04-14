3ab21914a28f29a0848165fc648897b4dfeed011
dnjswjd222 <bnm673@naver.com>
2025-04-14 11:04:39 +0900

    Add files via upload

--- shinhan/day09/lab34567/Prob1.java
+++ shinhan/day09/lab34567/Prob1.java
@@ -1,12 +1,33 @@
 package com.shinhan.day09.lab34567;
 
+import java.util.Scanner;
+
 public class Prob1 {
 	public static void main(String[] args) {
-		String str = "everyday we have is one more than we deserve";
-		String str2 = "";
-		
-		for(int i=0; i<str.length(); i++) {
-			
+		Scanner sc = new Scanner(System.in);
+		System.out.print("암호화할 문자열 : ");
+		String s = sc.nextLine();
+		System.out.print("암호화된 문자열 : ");
+		for (char c : s.toCharArray()) {
+			int temp = c + 3;
+
+			// 문자가 소문자 알파벳일 경우
+			if (c >= 'a' && c <= 'z') {
+				if (temp > 'z') { // 'z'를 초과하면 a~z 범위로 되돌림
+					temp -= 26;
+				}
+				System.out.print((char) temp);
+
+				// 문자가 대문자 알파벳일 경우
+			} else if (c >= 'A' && c <= 'Z') {
+				if (temp > 'Z') { // 'Z'를 초과하면 A~Z 범위로 되돌림
+					temp -= 26;
+				}
+				System.out.print((char) temp);
+
+			} else { // 알파벳이 아닌 문자(공백, 특수문자 등)는 그대로 출력
+				System.out.print(c);
+			}
 		}
 	}
 }
--- shinhan/day09/lab34567/StringComparator.java
+++ shinhan/day09/lab34567/StringComparator.java
@@ -8,4 +8,20 @@ public class StringComparator {
 		System.out.println(result1);// 12
 		System.out.println(result2);// 9
 	}
+	
+	private int compare(String str1, String str2, boolean isLength) {
+        if (isLength) {
+            return (str1.length() > str2.length() ? str1.length() : str2.length());
+        } else {
+            int count1 = 0;
+            int count2 = 0;
+            for (char a : str1.toCharArray()) {
+                if (a == 'a') count1++;
+            }
+            for(char a : str2.toCharArray()) {
+                if (a == 'a') count2++;
+            }
+            return (count1>count2) ? str1.length() : str2.length();
+        }
+    }
 }
